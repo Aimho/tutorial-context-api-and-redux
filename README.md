6669d2bc4969932c20a9a257203efbb31a694a71
AimHo <jjhwhwnsgh@gmail.com>
2020-04-04 19:12:08 +0900

    [update] counter redux refactoring using 'typesafe-actions'

--- README.md
+++ README.md
@@ -17,9 +17,21 @@ You will also see any lint errors in the console.
 velopert 블로그 - 리액트 프로젝트에서 타입스크립트 사용하기 5,6 실습 <br/>
 https://velog.io/@velopert/series/react-with-typescript
 
-redux code는 [https://github.com/erikras/ducks-modular-redux](Ducks패턴)을 사용함
+#### Ducks 패턴?
 
-##### Ducks패턴이란?
+redux code는 [Ducks패턴](https://github.com/erikras/ducks-modular-redux)을 사용함
 
 action type, create action function, reducer를 모두 한파일에 작성하는 방법으로, <br/>
 구조 중심이 아니라 기능(module) 중심으로 파일을 나누는 것
+
+Velopert Directory 작성방법
+
+```
+modules/
+  todos/
+    action.ts
+    index.ts
+    reducer.ts
+    types.td
+  counter.ts
+```
--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,25 +1,28 @@
+import { createAction, ActionType, createReducer } from "typesafe-actions";
+
 // action type 선언
 // as const는 const assertions이라는 TypeScript 문법 (type이 string이 되지 않고 실제 값을 가리킴)
-const INCREASE = "counter/INCREASE" as const;
-const DECREASE = "counter/DECREASE" as const;
-const INCREASE_BY = "counter/INCREASE_BY" as const;
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+const INCREASE_BY = "counter/INCREASE_BY";
 
 // create action function
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE)();
+// () => ({ type: INCREASE });
+export const decrease = createAction(DECREASE)();
+// () => ({ type: DECREASE });
+export const increaseBy = createAction(INCREASE_BY)<number>();
 // parameter 값을 payload 값으로 설정하는 것은 FSA 규칙을 따르기 위함
 // FSA 규칙 - 액션 객체 구조 일관성을 위한 규칙
-export const increaseBy = (diff: number) => ({
-  type: INCREASE_BY,
-  payload: diff,
-});
+// (payload: number) => ({ type: INCREASE_BY, payload });
 
 // action
-type TCounterAction =
-  // ReturnType은 함수에서 반환하는 타입을 가져올 수 있게 해주는 유틸타입 임
-  | ReturnType<typeof increase>
-  | ReturnType<typeof decrease>
-  | ReturnType<typeof increaseBy>;
+const actions = { increase, decrease, increaseBy };
+type TCounterAction = ActionType<typeof actions>;
+// ReturnType은 함수에서 반환하는 타입을 가져올 수 있게 해주는 유틸타입 임
+// | ReturnType<typeof increase>
+// | ReturnType<typeof decrease>
+// | ReturnType<typeof increaseBy>;
 
 // state & initialState
 type TCounterState = {
@@ -31,17 +34,22 @@ const initialState: TCounterState = {
 };
 
 // reducer
-function counter(state: TCounterState = initialState, action: TCounterAction) {
-  switch (action.type) {
-    case INCREASE:
-      return { count: state.count + 1 };
-    case DECREASE:
-      return { count: state.count - 1 };
-    case INCREASE_BY:
-      return { count: state.count + action.payload };
-    default:
-      return state;
-  }
-}
+const counter = createReducer<TCounterState, TCounterAction>(initialState, {
+  [INCREASE]: (state) => ({ count: state.count + 1 }),
+  [DECREASE]: (state) => ({ count: state.count - 1 }),
+  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload }),
+});
+// function counter(state: TCounterState = initialState, action: TCounterAction) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return { count: state.count + 1 };
+//     case DECREASE:
+//       return { count: state.count - 1 };
+//     case INCREASE_BY:
+//       return { count: state.count + action.payload };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
